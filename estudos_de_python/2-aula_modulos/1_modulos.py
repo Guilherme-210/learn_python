f9a1ec8d3320bfb31e3f93c9e447019d2117afb2
Guilherme-210 <guireis1164@gmail.com>
2024-07-23 09:08:41 -0300

    aula 1 modulo

---
+++ estudos_de_python/2-aula_modulos/1_modulos.py
@@ -0,0 +1,8 @@
+import calc
+from calc import div
+
+print(calc.sun(4 , 8))
+print(calc.sub(4 , 8))
+print(calc.mult(4 , 8))
+print(div(54 , 8))
+
---
+++ estudos_de_python/2-aula_modulos/desafios/string.py
@@ -0,0 +1,13 @@
+# Inverter uma string de trás pra frente.
+def inv(string):
+    return string[::-1]
+
+# Retornar apenas letras com índice par.
+def par(string):
+    return string[::2]
+    
+
+# Retornar apenas letras com índice ímpar.
+def imp(string):
+    return string[1::2]
+    
